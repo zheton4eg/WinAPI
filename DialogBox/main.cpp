711ccff86d4b6a10850a971de68bf9287eb3fb0e
zheka <zheka@HOME-PC>
2024-10-30 19:00:33 +0300

    classwork

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -14,6 +14,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+	}
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
